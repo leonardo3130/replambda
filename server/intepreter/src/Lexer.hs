c4cc0bb46ec5c58f5fedbe252b273b7272ff5aab
leonardo3130 <leonardopo313@gmail.com>
2025-11-04 22:15:20 +0100

    fix lexer

--- server/intepreter/src/Lexer.hs
+++ server/intepreter/src/Lexer.hs
@@ -2,23 +2,31 @@
 module Lexer where
 
 import Syntax
-import Data.Char (isSpace)
+import Data.Char (isSpace, isAlpha)
 
+data TokenType = Var | LPar | RPar | Lam | Dot | Space deriving (Enum, Show, Eq)
+data Token = Token TokenType String deriving (Show, Eq)
 
-data TokenType = Var | LPar | RPar | Lam | Dot | Space deriving (Enum)
-
--- Token = Token type + syntax symbol -> NO literal values
-data Token = Token TokenTypes String
+-- Wrapper 
+lexLambda :: String -> [Token]
+lexLambda s = tokenize s []
 
 -- Produces a list of token in the submitted Lambda calculus program
-tokenize :: String -> [Token]
+tokenize :: String -> String -> [Token]
 tokenize [] [] = []
-tokenize [] currentVar = (Token Var (reverse currentVar)) : []
-tokenize ('\\' : end) currentVar = (Token Lam "\\") : tokenize end currentVar 
-tokenize (' ' : end) [] = (Token Space " ") : tokenize end []
-tokenize (' ' : end) currentVar = (Token Var currentVar) : (Token Space " ") : tokenize end []
-tokenize ('(' : end) currentVar = (Token LPar "("): tokenize end currentVar
-tokenize (')' : end) currentVar = (Token RPar ")"): tokenize end currentVar
-tokenize ('.' : end) currentVar = (Token Dot "."): tokenize end
+tokenize [] currentVar = [Token Var (reverse currentVar)]
+tokenize ('\\' : end) [] = Token Lam "\\" : tokenize end []
+tokenize ('\\' : end) currentVar = Token Var (reverse currentVar) : Token Lam "\\" : tokenize end []
+tokenize (' ' : end) [] = Token Space " " : tokenize end []
+tokenize (' ' : end) currentVar = Token Var (reverse currentVar) : Token Space " " : tokenize end []
+tokenize ('(' : end) [] = Token LPar "(" : tokenize end []
+tokenize ('(' : end) currentVar = Token Var (reverse currentVar) : Token LPar "(" : tokenize end []
+tokenize (')' : end) [] = Token RPar ")" : tokenize end []
+tokenize (')' : end) currentVar = Token Var (reverse currentVar) : Token RPar ")" : tokenize end []
+tokenize ('.' : end) [] = Token Dot "." : tokenize end []
+tokenize ('.' : end) currentVar = Token Var (reverse currentVar) : Token Dot "." : tokenize end []
 tokenize (x:end) currentVar
   | isAlpha x = tokenize end (x:currentVar)
+  | otherwise = if null currentVar
+                  then tokenize end []
+                  else Token Var (reverse currentVar) : tokenize end []
