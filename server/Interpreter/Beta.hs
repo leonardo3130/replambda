ff8cdb92aff9b33f68edbf20d7cae4773d9b7057
leonardo3130 <leonardopo313@gmail.com>
2025-11-18 08:19:15 +0100

    add beta reuction step function

--- server/Interpreter/Beta.hs
+++ server/Interpreter/Beta.hs
@@ -10,3 +10,22 @@ betaReduce (Application (Abstraction (NodeVar v) t2) t1) = substitute v t1 t2
 betaReduce (Application t1 t2) = Application (betaReduce t1) (betaReduce t2)
 betaReduce (Abstraction (NodeVar v) t) = Abstraction (NodeVar v) (betaReduce t)
 betaReduce varnode@(NodeVar _) = varnode
+
+-- performs one step of beta reduction (when possible)
+betaStep :: AST -> Maybe AST
+betaStep ast =
+  case ast of
+    Application (Abstraction (NodeVar v) t2) t1 -> Just (substitute v t1 t2)
+    Application t1 t2 ->
+      case betaStep t1 of
+        Just t1' -> Just (Application t1' t2)
+        Nothing ->
+          -- only when no reduction in t1, try t2 --> one step only
+          case betaStep t2 of
+            Just t2' -> Just (Application t1 t2')
+            Nothing -> Nothing
+    Abstraction var@(NodeVar v) t ->
+      case betaStep t of
+        Just t' -> Just (Abstraction var t')
+        Nothing -> Nothing
+    NodeVar _ -> Nothing
