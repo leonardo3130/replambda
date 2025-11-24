ccf0ecb1dbf144f09d814398ce0c82c900c22602
leonardo3130 <leonardopo313@gmail.com>
2025-11-25 00:46:57 +0100

    add command history

--- client/src/components/REPL.tsx
+++ client/src/components/REPL.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Terminal, {
   ColorMode,
   TerminalOutput,
@@ -10,41 +10,74 @@ export const TerminalController = () => {
   const [terminalLineData, setTerminalLineData] = useState([
     <TerminalOutput key={-1}>Welcome !</TerminalOutput>,
   ]);
-  const [terminalHistory, setTerminalHistory] = useState([
-    <TerminalOutput key={-1}>Welcome !</TerminalOutput>,
-  ]);
+  const [terminalHistory, setTerminalHistory] = useState<string[]>([]);
+  const [inputValue, setInputValue] = useState<string>("");
+  const [historyIndex, setHistoryIndex] = useState<number>(-1);
+
+  const increaseHistoryIndex = () => {
+    setHistoryIndex((prevIndex) =>
+      Math.min(prevIndex + 1, terminalHistory.length - 1),
+    );
+
+    setInputValue(
+      terminalHistory[Math.min(historyIndex + 1, terminalHistory.length - 1)] ||
+      "",
+    );
+  };
+
+  useEffect(() => {
+    setTerminalLineData((prevData) => [
+      ...prevData.filter(
+        (line) => !(line.type === TerminalInput && line.key === "current"),
+      ),
+      <TerminalInput key="current">{inputValue}</TerminalInput>,
+    ]);
+  }, [inputValue]);
+
+  const decreaseHistoryIndex = () => {
+    setHistoryIndex((prevIndex) => Math.max(prevIndex - 1, -1));
+
+    setInputValue(terminalHistory[Math.max(historyIndex - 1, -1)] || "");
+  };
 
   const handleInput = async (input: string) => {
     input = input.replaceAll("λ", "\\").trim(); // Replace λ with \\ for backend compatibility  ;
     let ld = [...terminalLineData];
     if (input.toLocaleLowerCase().trim() === "clear") {
-      setTerminalHistory((prev) => [...prev, ...ld]);
+      setTerminalHistory((history) => [...history, "clear"]);
       ld = [];
     } else if (input.toLocaleLowerCase().trim() === "history") {
+      setTerminalHistory((history) => [...history, "history"]);
       ld.push(<TerminalInput key={ld.length}>{input}</TerminalInput>);
-      setTerminalHistory((prev) => [...prev, ...ld]);
       for (let i = 0; i < terminalHistory.length; i++) {
         const line = terminalHistory[i];
-        if (line.type === TerminalInput) {
-          ld.push(
-            <TerminalOutput key={terminalHistory.length + i}>
-              {line.props.children}
-            </TerminalOutput>,
-          );
-        }
+        ld.push(
+          <TerminalOutput key={terminalHistory.length + i}>
+            {line}
+          </TerminalOutput>,
+        );
       }
     } else {
       const reduced = await apiManagerInstance.fullReduce(input);
       console.log(reduced);
       ld.push(<TerminalInput key={ld.length}>{input}</TerminalInput>);
       ld.push(<TerminalOutput key={ld.length + 1}>{input}</TerminalOutput>);
+      setTerminalHistory((history) => [...history, input]);
     }
     setTerminalLineData(ld);
   };
 
-  // Terminal has 100% width by default, so it should usually be wrapped in a container div
   return (
-    <div className="terminal-container">
+    <div
+      className="terminal-container"
+      onKeyDown={(e) => {
+        if (e.key === "ArrowUp") {
+          decreaseHistoryIndex();
+        } else if (e.key === "ArrowDown") {
+          increaseHistoryIndex();
+        }
+      }}
+    >
       <Terminal
         name="λ-Calculus REPL "
         colorMode={ColorMode.Dark}
